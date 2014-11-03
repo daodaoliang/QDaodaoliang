878a14b0cf47bfb1a2e0c51af6fa3ecc5c0ce96b
daodaoliang <daodaoliang@yeah.net>
2014-11-03 17:31:00 +0800

    提交代码修改

--- QComputerInfo/src/qcomputerinfo.cpp
+++ QComputerInfo/src/qcomputerinfo.cpp
@@ -52,6 +52,16 @@ QString QComputerInfo::getHardDiskSerialNumber(bool &param_bool)
 
 QString QComputerInfo::getCPUCount(bool &param_bool)
 {
+    //备用(2014年11月3日--daodaoliang)
+    //systemInfo.dwActiveProcessorMask ---处理器掩码
+    //systemInfo.dwNumberOfProcessors ---处理器个数
+    //systemInfo.dwPageSize ---处理器分页大小
+    //systemInfo.dwProcessorType ---处理器类型
+    //systemInfo.lpMaximumApplicationAddress ---最大寻址单元
+    //systemInfo.lpMinimumApplicationAddress ---最小寻址单元
+    //systemInfo.wProcessorLevel ---处理器等级
+    //systemInfo.wProcessorRevision ---处理器版本
+
     SYSTEM_INFO systemInfo;
     GetSystemInfo(&systemInfo);
     param_bool = true;
